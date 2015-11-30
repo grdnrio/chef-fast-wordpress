d689e88c94465228f8cdcebcf1048651dbd81bf8
Joe Gardiner <joe@joegardiner.co.uk>
2015-11-30 21:59:07 +0000

    Added aptitude upstream cookbook

--- cookbooks/chef-wordpress/metadata.rb
+++ cookbooks/chef-wordpress/metadata.rb
@@ -5,3 +5,5 @@ license 'all_rights'
 description 'Installs/Configures the fastest WordPress in the west'
 long_description 'Build a WordPress hosting stack on a single node using Nginx, HHVM (PHP), MariadDB and a user specified version of WordPress'
 version '0.1.0'
+
+depends 'apt'
