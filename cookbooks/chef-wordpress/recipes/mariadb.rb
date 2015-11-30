d74f6c3a14faae7d3e7a77fb7864388330e0ad43
Joe Gardiner <joe@joegardiner.co.uk>
2015-11-30 21:54:08 +0000

    Created scratchpad recipes

---
+++ cookbooks/chef-wordpress/recipes/mariadb.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef-wordpress
+# Recipe:: mariadb
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
---
+++ cookbooks/chef-wordpress/recipes/wordpress.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef-wordpress
+# Recipe:: wordpress
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
