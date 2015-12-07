f61266f994574debc105c2fcfc4eb005940f0b58
Joe Gardiner <joe@joegardiner.co.uk>
2015-12-07 16:12:04 +0000

    Apt version pinning

--- cookbooks/chef-wordpress/metadata.rb
+++ cookbooks/chef-wordpress/metadata.rb
@@ -6,4 +6,4 @@ description 'Installs/Configures the fastest WordPress in the west'
 long_description 'Build a WordPress hosting stack on a single node using Nginx, HHVM (PHP), MariadDB and a user specified version of WordPress'
 version '0.1.0'
 
-depends 'apt'
+depends 'apt', '~> 2.9.2'
