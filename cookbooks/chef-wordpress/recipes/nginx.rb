a23f2df4d795163fa883f787a960defabad370ee
Joe Gardiner <joe@joegardiner.co.uk>
2015-12-07 16:10:47 +0000

    Install Nginx, chkconfig on and import template file (default nginx.conf)

--- cookbooks/chef-wordpress/recipes/nginx.rb
+++ cookbooks/chef-wordpress/recipes/nginx.rb
@@ -3,3 +3,23 @@
 # Recipe:: nginx
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+# Install Nginx
+package 'nginx' do
+  action :install
+end
+
+# Enable at boot
+service 'nginx' do
+  action :enable
+end
+
+# Create cofig file from template
+template 'nginx.conf' do
+  path "#{node['nginx']['dir']}/nginx.conf"
+  source 'nginx.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  notifies :restart, resources(:service => 'nginx')
+end
